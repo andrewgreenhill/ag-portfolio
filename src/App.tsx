14505814a0998d011524fe1b518ed273a227985c
andrewgreenhill <andrewgreenhill@users.noreply.github.com>
2025-01-30 10:35:28 +0800

    Initial config: Prettier, Title, Logo, Doco, etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
   return (
@@ -40,4 +40,4 @@ function App() {
 //   )
 // }
 
-export default App
+export default App;
